00bef9cfedc5b8300d2d0f07a018271a20f41049
Devin Riley <devin.riley144@gmail.com>
2022-12-30 11:18:02 -0600

    finished program, added cases

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Whats is your favorite subject in school?");
+            var faveSubject = Console.ReadLine();
+
+            switch (faveSubject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is a tough one!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is fun!");
+                    break;
+                case "english":
+                    Console.WriteLine("You'll do a lot of reading in this one!");
+                    break;
+                case "history":
+                    Console.WriteLine("Flashcards will be very beneficial in this subject");
+                    break;
+                case "physical Education":
+                case "p.e.":
+                case "pe":
+                case "gym":
+                    Console.WriteLine("Great time to get some energy out!");
+                    break;
+                default: 
+                    Console.WriteLine($"I'm sorry, I'm not familiar with {faveSubject}.");
+                    break;
+            }         
+         
         }
     }
 }
